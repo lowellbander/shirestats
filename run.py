c01f22ad95e8730949daa28db90442eb536cf709
Lowell Bander <lowellbander@gmail.com>
2014-12-26 22:27:20 -0800

    trapezius

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app.debug = True
 
 @app.route('/')
 def index():
-    return "hello world"
+    return "Under construction, at the order of Baevid Trapezius Guo."
 
 if __name__ == '__main__':
     app.run()
